6d58d71f729a52f3f3fb4cd54c9c19e1338d33ae
Abhishek Parmar <abhishek205parmar@gmail.com>
2026-04-15 11:43:14 +0530

    Fix CORS: Add Vercel frontend origin

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL,"https://mini-project-2-2zep.onrender.com "],// or 3000 depending on frontend
+    origin: [process.env.CLIENT_URL, "https://mini-project-2-2zep.onrender.com", "https://mini-project-beryl-pi.vercel.app"],// or 3000 depending on frontend
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
